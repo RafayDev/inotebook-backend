7da790dee679bec1e89b35633b452a5cd30eeab9
rafay9475 <rafay9475@gmail.com>
2022-08-13 17:33:04 +0500

    some validation and error catches

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,36 @@
 const express=require('express');
 const User = require('../models/User');
 const router= express.Router();
-//Create a User Using: POST Method "api/auth/"
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    const user=User(req.body);
-    user.save();
-    res.send(req.body);
+const { body, validationResult } = require('express-validator');
+//Create a User Using: POST Method "api/auth/createuser"
+router.post('/createuser',[
+    body('name','Enter Name').not().isEmpty(),
+    body('email','Enter Valid Email').isEmail(),
+    body('password','Password Must be  6 lenght').isLength({min:6})
+],async(req,res)=>{
+    //if the validation is not passed
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+// cheack email is already exist
+ try{
+    let user=await User.findOne({email:req.body.email});
+    if(user){
+        return res.status(400).json({errors:[{msg:'Email Already Exist'}]});
+    }
+
+     user= await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      res.json("User Successfully Created");
+ }
+ catch(error){
+  console.error(error.message);
+  res.status(500).send("Some Error")
+ }
 }
 );
 module.exports=router;
